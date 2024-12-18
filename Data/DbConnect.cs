8a30cdf22c6db562ba8bdd6e897d3ea2ddc2463b
Filipe Lauren <filipe.lauren@fiscal.io>
2024-12-18 11:09:01 -0300

    Criando classe de conexão com o banco de dados padrão SingleTon e primeira inserção de dados.

---
+++ Data/DbConnect.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using Dapper;
+using System;
+using System.Data;
+using System.Collections.Generic;
+
+public class DbConnect
+{
+    // Instância estática para o Singleton
+    private static readonly Lazy<NpgsqlConnection> _instance =
+        new Lazy<NpgsqlConnection>(() => 
+        new NpgsqlConnection("Host=localhost;Database=db_teste_crud;Username=postgres;Password=1234;Port=5432"));
+
+    // Construtor privado para evitar instância externa
+    private DbConnect() { }
+
+    // Propriedade pública que acessa a instância da conexão
+    public static NpgsqlConnection GetConnection()
+    {
+        return _instance.Value;
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,28 @@ namespace testecrudfiscalio
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // Pegando a conexão com o banco
+            var conn = DbConnect.GetConnection();
+            try
+            {
+                //  Preenchendo os dados
+                var notaFiscal = new NotaFiscal
+                {
+                    IdNota = 1,
+                    Emissor = "Teste",
+                    Data = DateTime.Now
+                };
+                // query de inserção de de dados
+                string sql = "insert into notafiscal (idnota, emissor, data) values (@IdNota,@Emissor, @Data)";
+                // passando qual a query e qual a model
+                conn.Execute(sql, notaFiscal);
+            }
+            finally
+            {
+                // independente de qualquer coisa, sempre fechar a conexão com o banco de dados
+                conn.Close();
+            }
         }
     }
 }
